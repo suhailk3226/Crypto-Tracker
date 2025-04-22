b3cddc6fae98eb004dcb838a533178ee4be70296
suhail <suhailkhan3226@gmail.com>
2025-04-22 20:22:49 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Coin from "./pages/Coin";
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Coin from './pages/Coin';
 
 const App = () => {
   return (
     <div className="app w-full min-h-screen">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/crypto-tracker" element={<Home />} />
         <Route path="/coin/:coinId" element={<Coin />} />
       </Routes>
     </div>
